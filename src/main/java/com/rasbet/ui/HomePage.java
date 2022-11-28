327c60e00aa44e7ea9f96edb2ac9b3c8d4da21d5
BenjamimCosta <73348441+BenjamimCosta@users.noreply.github.com>
2022-11-28 17:05:51 +0000

    historico de apostas incompleto,profile feito

--- src/main/java/com/rasbet/ui/HomePage.java
+++ src/main/java/com/rasbet/ui/HomePage.java
@@ -6,9 +6,11 @@ import com.rasbet.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class HomePage {
@@ -21,4 +23,32 @@ public class HomePage {
         model.addAttribute("games", RasBetFacade.games);
         return view;
     }
+
+    @GetMapping("/profile")
+    public String showProfile(Model model) {
+        String view = "profile";
+        if(RasBetFacade.emailAuthenticatedUser == null) view="redirect:login";
+        model.addAttribute("user", RasBetFacade.getAuthenticatedUser());
+        return view;
+    }
+    @GetMapping("/saveChanges")
+    public String showSaveChanges(Model model, @RequestParam String nome,@RequestParam String email,@RequestParam String password) {
+        String view = "redirect:profile";
+        if(RasBetFacade.emailAuthenticatedUser == null) view="redirect:login";
+        if(Objects.equals(password, "")) {
+            RasBetFacade.changeInfo(nome,email);
+        }
+        else RasBetFacade.changeInfo(nome, password, email);
+        model.addAttribute("user", RasBetFacade.getAuthenticatedUser());
+        return view;
+    }
+    @GetMapping("/betHistory")
+    public String showBetHistory(Model model) {
+        String view = "betHistory";
+        if(RasBetFacade.emailAuthenticatedUser == null) view="redirect:login";
+        model.addAttribute("user", RasBetFacade.getAuthenticatedUser());
+        model.addAttribute("bets", RasBetFacade.getBetListEmail(RasBetFacade.emailAuthenticatedUser));
+        return view;
+    }
+
 }
